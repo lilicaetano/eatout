38501e3b075a489c71dbc57ef6436eca47503261
Alexander Stones <amstones@hotmail.co.uk>
2019-08-31 11:28:38 +0100

    added some restaurant data and seed creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+RESTAURANTS = [
+ ["Pret A Manger", "24 Farringdon St", "EC4A 4AB"],
+ ["Pure", "3 St Bride St", "EC4A 4AS"],
+ ["Leon", "12 Ludgate Circus", "EC4M 7LQ"],
+ ["Shot", "23 Bride Ln", "EC4Y 8DT"],
+ ["Beboz Italian Street Food", "60C Holborn Viaduct", "EC1A 2FD"],
+ ["The Fable", "52 Holborn Viaduct", "EC1A 2FD"],
+ ["Vivat Bacchus", "47 Farringdon St", "EC4A 4LL"],
+ ["City Of  Distillery", "22-24 Bride Ln", "EC4Y 8DT"],
+ ["McDonald's", "152/153 Fleet St", "EC4A 2DQ"],
+ ["Ye Olde Cheshire Cheese", "145 Fleet St", "EC4A 2BU"]
+]
+
+
+def restaurant_hasher(nested_array)
+  nested_array.each do |restaurant|
+    newRestaurant = Restaurant.create!(name: restaurant[0])
+  end
+end
+
+
 require 'faker'
 
 puts 'Seed start'
@@ -18,4 +39,10 @@ counter = 0
   counter += 1
 end
 
+restaurant_hasher(RESTAURANTS)
+
+
+
 puts 'Seed end'
+
+
