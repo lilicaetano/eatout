b8e2c1e2324890be0883697b1a6da321aaf65795
Alexander Stones <amstones@hotmail.co.uk>
2019-09-10 18:56:57 +0100

    added user location

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -16,10 +16,46 @@ const initMapbox = () => {
       style: 'mapbox://styles/lordmoomoo/ck0705ics06cn1cm50aeuz50k'
     });
 
-    const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+    // let currentLocation = {};
+    // navigator.geolocation.getCurrentPosition(function(position) {
+    //   let data = {
+    //     lat: position.coords.latitude,
+    //     lng: position.coords.longitude,
+    //     image_url: 'https://banner2.kisspng.com/20180509/klq/kisspng-person-logo-computer-icons-5af2c2026fcf79.460327751525858818458.jpg',
+    //     infoWindow: ''
+    //     };
+    //   //console.log(data, 'data');
+    //   Object.assign(currentLocation, data);
+    //   //currentLocation.push(data);
+    //   //console.log(currentLocation, 'oof');
+    // });
 
+    map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true
+    }));
+
+    const directionFetch = (usrLat, usrLng, restLat, restLng) => {
+      fetch(`https://api.mapbox.com/directions/v5/mapbox/walking/${usrLat},${usrLng};${restLat},${restLng}?geometries=geojson&access_token=pk.eyJ1IjoibG9yZG1vb21vbyIsImEiOiJjazA0Nnc0YTQzMmNoM2d1dGJ1djVxd3dyIn0.pGdCWeFobIL8YR1KwhK3Vw`, {
+      //method: "POST"
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+    };
+
+    directionFetch(51.5151385, 0.115524, 51.5151385, 0.105234);
+    //console.log(currentLocation, 'dog');
+    const markers = JSON.parse(mapElement.dataset.markers);
+    //markers.push(currentLocation);
+    //console.log(markers);
+    markers.forEach((marker) => {
+      console.log(marker.lat, 'lat');
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+      console.log(marker, 'marker')
       const element = document.createElement('div');
       element.className = 'marker';
       element.style.backgroundImage = `url('${marker.image_url}')`;
@@ -33,7 +69,10 @@ const initMapbox = () => {
       .addTo(map);
     });
 
+
     fitMapToMarkers(map, markers);
+
+
   }
 
 
