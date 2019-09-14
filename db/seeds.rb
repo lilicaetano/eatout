705331a2a178ca9c5a7cf8a91b2b4f9c161fc5cd
Tomas Dillon <tom.dillon1@icloud.com>
2019-09-14 15:18:14 +0100

    Added restaurant images.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 RESTAURANTS = [
- ["Pret A Manger", "24 Farringdon St", "EC4A 4AB", "https://i2.wp.com/www.breakfastinabag.co.uk/wp-content/uploads/2016/11/Pret-A-Manger-Logo-2.jpg"],
+ ["Pret A Manger", "24 Farringdon St", "EC4A 4AB", "https://i.pinimg.com/originals/12/1a/e0/121ae05c213b2c37522e7fde2f4d8263.jpg"],
  ["Pure", "3 St Bride St", "EC4A 4AS", "https://www.pure.co.uk/wp-content/uploads/cropped-PURE_FAVICON_PLAIN-100.jpg"],
  ["Leon", "12 Ludgate Circus", "EC4M 7LQ", "https://image.isu.pub/141007162921-0065e5caf295d851ea489111dc67fa57/jpg/page_1.jpg"],
  #["Shot", "23 Bride Ln", "EC4Y 8DT"],
  ["Beboz Italian Street Food", "60C Holborn Viaduct", "EC1A 2FD", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3dNYGeGkD1RCAdxSSJNzt2jK_l8PzbuoPQqbzDyTS6gI2LcMS"],
  #["The Fable", "52 Holborn Viaduct", "EC1A 2FD"],
  ["Vivat Bacchus", "47 Farringdon St", "EC4A 4LL", "https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/10599543_10154640129505158_8096655893224581267_n.jpg?_nc_cat=100&_nc_oc=AQkSEyPoq7uxBzPBhgeoevIWcVw2eKq2JrV7Wh6F2Ngo-5JlXHr4L7mKEM4qyDMk5IQ&_nc_ht=scontent-lhr3-1.xx&oh=493edadd7f4ab2c5b0912383b5dd95d8&oe=5E3CAD54"],
- ["City Of  Distillery", "22-24 Bride Ln", "EC4Y 8DT", "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj_4ZSls9DkAhUuyIUKHRaZBT4QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.cityoflondondistillery.com%2F&psig=AOvVaw2M7Sy_SMJsh-dRbZOhPwf0&ust=1568553496887068"],
+ ["City Of London Distillery", "22-24 Bride Ln", "EC4Y 8DT", "https://halewood-int.com/wp-content/uploads/2017/09/logo-cold.jpg"],
  ["McDonald's", "152/153 Fleet St", "EC4A 2DQ", "https://miro.medium.com/max/2000/1*sszpZOih_xJV_lZsDbog-Q.png"],
  ["Ye Olde Cheshire Cheese", "145 Fleet St", "EC4A 2BU", "https://static.standard.co.uk/s3fs-public/thumbnails/image/2017/12/11/11/cheshire-cheese.jpg?w968"]
 ]
