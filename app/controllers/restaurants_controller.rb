85ebbbc01aca372b99b0cf8ed9acf5eae030fffe
Alexander Stones <amstones@hotmail.co.uk>
2019-09-14 13:10:00 +0100

    added some logic to inform user if they have created a meeting at the chosen point

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,14 +2,18 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.geocoded
     meetingPlaces = Restaurant.joins(:meetings).uniq
+
     @markers = @restaurants.map do |restaurant|
       if meetingPlaces.include?(restaurant)
           img = helpers.asset_url('green-pin.png')
           meetings_available = true
+          @own_meeting = own_meeting_check(restaurant)
         else
           img = helpers.asset_url('pink-map-pin.png')
           meetings_available = false
+          own_meeting = false
       end
+
       {
         lat: restaurant.latitude,
         lng: restaurant.longitude,
@@ -18,6 +22,7 @@ class RestaurantsController < ApplicationController
       }
 
     end
+
   end
 
   def show
@@ -28,4 +33,25 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     @meetings = Meeting.where(restaurant: @restaurant)
   end
+
+  private
+
+  def own_meeting_check(restaurant)
+    @user_meetings = Meeting.where(user: current_user)
+    @user_guest = Guest.where(user: current_user)
+
+    found = false
+    @user_meetings.each do |meeting|
+      if meeting.restaurant == restaurant
+        found = true
+      end
+    end
+    @user_guest.each do |is_guest|
+      if is_guest.meeting.restaurant == restaurant
+        found = true
+      end
+    end
+    return found
+  end
+
 end
