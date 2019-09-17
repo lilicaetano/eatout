8eb8a64252efbad363d6b063c66c4045591a367e
Tomas Dillon <tom.dillon1@icloud.com>
2019-09-17 20:01:12 +0100

    Added new seed data to restaurants.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,12 @@ RESTAURANTS = [
  ["Pure", "3 St Bride St", "EC4A 4AS", "https://www.pure.co.uk/wp-content/uploads/cropped-PURE_FAVICON_PLAIN-100.jpg"],
  ["Leon", "12 Ludgate Circus", "EC4M 7LQ", "https://image.isu.pub/141007162921-0065e5caf295d851ea489111dc67fa57/jpg/page_1.jpg"],
  #["Shot", "23 Bride Ln", "EC4Y 8DT"],
- ["Beboz Italian", "60C Holborn Viaduct", "EC1A 2FD", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3dNYGeGkD1RCAdxSSJNzt2jK_l8PzbuoPQqbzDyTS6gI2LcMS"],
+ ["Obicà", "7 Limeburner Ln", "EC4M 7AX", "https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/38301047_1742384412542237_3383676485347835904_n.png?_nc_cat=108&_nc_oc=AQnLPHQoTkf72bgDsh98XNmb7gCmcQlyOxzXm8eDMwhefFO39Ah_DAvlDB4Z7_axFhk&_nc_ht=scontent-lhr3-1.xx&oh=b85822455d3aa57b33bb3206532a9fd5&oe=5DF76575"],
  #["The Fable", "52 Holborn Viaduct", "EC1A 2FD"],
- ["Vivat Bacchus", "47 Farringdon St", "EC4A 4LL", "https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/10599543_10154640129505158_8096655893224581267_n.jpg?_nc_cat=100&_nc_oc=AQkSEyPoq7uxBzPBhgeoevIWcVw2eKq2JrV7Wh6F2Ngo-5JlXHr4L7mKEM4qyDMk5IQ&_nc_ht=scontent-lhr3-1.xx&oh=493edadd7f4ab2c5b0912383b5dd95d8&oe=5E3CAD54"],
- ["City Distillery", "22-24 Bride Ln", "EC4Y 8DT", "https://www.cityoflondondistillery.com/wp-content/uploads/2015/09/logo.svg"],
- ["McDonald's", "152/153 Fleet St", "EC4A 2DQ", "https://miro.medium.com/max/2000/1*sszpZOih_xJV_lZsDbog-Q.png"],
- ["Ye Olde Cheshire Cheese", "145 Fleet St", "EC4A 2BU", "https://static.standard.co.uk/s3fs-public/thumbnails/image/2017/12/11/11/cheshire-cheese.jpg?w968"]
+ ["itsu", "130 Fleet St", "EC4A 2BH", "https://www.afroditi.com/wp-content/uploads/2015/02/afroditi-retaildesign-itsu17.jpg"],
+ ["Pizza Express", "1 New Fetter Ln", "EC4A 1AN", "https://upload.wikimedia.org/wikipedia/en/thumb/6/69/PizzaExpressBlack.svg/1200px-PizzaExpressBlack.svg.png"],
+ ["Chilango", "142 Fleet St", "EC4A 2DQ", "https://chilango.co.uk/wp-content/themes/wpbootstrap/img/logo/chilangoLogoCircle.svg"],
+ ["Café Rouge", "14 St. Paul's Churchyard", "EC4M 8AY", "https://s3-media3.fl.yelpcdn.com/bphoto/asCExm9Lh18RdTJB7lw7Jw/o.jpg"]
 ]
 
 USERS = [
