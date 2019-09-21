3551ce2f16dc398e655d43628f35ac973f8c9fe9
Alexander Stones <amstones@hotmail.co.uk>
2019-09-21 14:15:47 +0100

    now shows user created meeting messages

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -2,14 +2,13 @@ class ChatRoomsController < ApplicationController
   def index
     @meetings = Meeting.where(user: current_user)
     @user_guest = Guest.where(user: current_user)
-    # @any_created_or_booked = @meetings + @user_guest
     @chat_rooms = []
     @user_guest.each do |guest|
       @chat_rooms << ChatRoom.find_by(guest: guest)
     end
-    # @meetings.each do |user|
-    #   @chat_rooms << ChatRoom.find_by(guest: Guest.find_by)
-    # end
+    @meetings.each do |meeting|
+      @chat_rooms << ChatRoom.find_by(guest: Guest.find_by(meeting: meeting))
+    end
   end
 
   def show
