b1ceab72972b9a36000382c1d25b41bcc0997a55
Alexander Stones <amstones@hotmail.co.uk>
2019-09-21 14:01:40 +0100

    started logic

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,11 +1,15 @@
 class ChatRoomsController < ApplicationController
   def index
-    @guests = Guest.where(user: current_user)
-
+    @meetings = Meeting.where(user: current_user)
+    @user_guest = Guest.where(user: current_user)
+    # @any_created_or_booked = @meetings + @user_guest
     @chat_rooms = []
-    @guests.each do |guest|
+    @user_guest.each do |guest|
       @chat_rooms << ChatRoom.find_by(guest: guest)
     end
+    # @meetings.each do |user|
+    #   @chat_rooms << ChatRoom.find_by(guest: Guest.find_by)
+    # end
   end
 
   def show
