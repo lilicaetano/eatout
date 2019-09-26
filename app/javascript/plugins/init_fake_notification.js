d24e69dc40c9cc4e4a6f20187fb02fc06aa376d4
Alexander Stones <amstones@hotmail.co.uk>
2019-09-26 13:36:10 +0100

    css changes

--- app/javascript/plugins/init_fake_notification.js
+++ app/javascript/plugins/init_fake_notification.js
@@ -3,10 +3,13 @@ import 'jquery';
 const initFakeNotify = () => {
   let foo = $('#fake-notification');
   if (foo){
+    foo.hide();
     setTimeout(function(){
-      foo.html('<h2>fooooo</h2>');
-      foo.class('')
-    }, 2);
+
+      foo.append('<h2>fooooo</h2>');
+      foo.addClass('fake');
+      foo.fadeIn(500);
+    }, 1000);
   }
 }
 
